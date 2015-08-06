a24c3bc781af6d513eb428d2a5449e430fe8c4c4
Hongbo Jing <jack.jing7@gmail.com>
2015-08-06 01:40:04 -0400

    day 41 simple demo to config

--- builds/development/js/lib/angular/app.js
+++ builds/development/js/lib/angular/app.js
@@ -1,5 +1,11 @@
 angular.module('myApp',[])
 .controller('CalendarController',function($scope){
-
+  $scope.tab=1;
+  $scope.setTab=function(param){
+    $scope.tab=param;
+  };
+  $scope.isSet=function(param){
+    return $scope.tab===param;
+  };
 })
 ;
